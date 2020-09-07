a8cce2ad40ea3b712818ffb1cb6e50989b262fdd
davidlei <davidleitw@gmail.com>
2020-09-07 11:03:07 +0800

    main

--- main.go
+++ main.go
@@ -7,17 +7,15 @@ import (
 
 func main() {
 	server := gin.Default()
-	// server.GET("/callback", backend.GoogleCallBack)
-	// server.GET("/Hello", func(ctx *gin.Context) {
-	// 	ctx.String(200, "Hello world")
-	// })
-	// server.GET("/test", backend.GoogleOauthLogin)
 
+	// oauth2 group, if you want to login, visit these routes.
 	oauth := server.Group("oauth")
 	{
 		oauth.GET("/google", backend.GoogleOauthLogin)
 		oauth.GET("/facebook", backend.FacebookOauthLogin)
 	}
+
+	// callback group
 	callback := server.Group("callback")
 	{
 		callback.GET("/google", backend.GoogleCallBack)
