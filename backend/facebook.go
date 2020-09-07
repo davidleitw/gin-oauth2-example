369019dfe56fed9ab59bcd3c5001ad67961a88e1
davidlei <davidleitw@gmail.com>
2020-09-07 11:03:55 +0800

    facebook oauth api test

--- backend/facebook.go
+++ backend/facebook.go
@@ -1,13 +1,56 @@
 package backend
 
 import (
+	"errors"
+	"fmt"
+	"net/http"
+
 	"github.com/gin-gonic/gin"
+	"golang.org/x/oauth2"
+	"golang.org/x/oauth2/facebook"
 )
 
-func FacebookOauthLogin(ctx *gin.Context) {
+var facebook_config *oauth2.Config
 
+func getFacebookOauthURL() string {
+	options := CreateClientOptions("facebook")
+
+	facebook_config = &oauth2.Config{
+		ClientID:     options.getID(),
+		ClientSecret: options.getSecret(),
+		RedirectURL:  "https://ginoauth-example.herokuapp.com/callback/facebook",
+		Scopes: []string{
+			"email",
+		},
+		Endpoint: facebook.Endpoint,
+	}
+
+	return facebook_config.AuthCodeURL("FaceBook")
+}
+
+func FacebookOauthLogin(ctx *gin.Context) {
+	redirectURL := getFacebookOauthURL()
+	ctx.Redirect(http.StatusSeeOther, redirectURL)
 }
 
 func FacebookCallBack(ctx *gin.Context) {
+	if error_reason := ctx.Query("error_reason"); error_reason != "" {
+		_ = ctx.AbortWithError(http.StatusUnauthorized, errors.New(error_reason))
+		return
+	}
 
+	state := ctx.Query("state")
+	if state != "Facebook" {
+		_ = ctx.AbortWithError(http.StatusUnauthorized, StateError)
+		return
+	}
+
+	code := ctx.Query("code")
+	token, err := facebook_config.Exchange(ctx, code)
+	if err != nil {
+		_ = ctx.AbortWithError(http.StatusUnauthorized, err)
+		return
+	}
+
+	fmt.Println("token = ", token)
 }
